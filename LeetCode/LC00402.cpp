87407b6e191da46fb8be04c6f71322130f67e6e2
Sunrisepeak <speakshen@163.com>
2021-02-22 22:07:45 +0800

    modify filename & greedy & stack & double pointer

---
+++ LeetCode/LC00402.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        int stackTop = 0, j = 0;
+        for (; j < num.size(); j++) {
+            while (k && stackTop > 0 && num[stackTop - 1] > num[j]) {
+                stackTop--; k--;
+            }
+            num[stackTop++] = num[j];
+        }
+        stackTop -= k;
+        int preZeroLen { 0 };
+        while (preZeroLen < stackTop && num[preZeroLen] == '0') preZeroLen++;
+        return preZeroLen == stackTop ? "0" : num.substr(preZeroLen, stackTop - preZeroLen);
+    }
+};
+
+/**
+ *
+ *  1.single stack
+ *  2.greedy
+ *  3.double pointer
+ *
+ * */
